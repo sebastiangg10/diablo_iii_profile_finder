da500de4497064ff7aab4f99808bf15add50a36d
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-07-29 12:57:11 -0500

    Instalación de librerias y validacion de Bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+// Paquetes de npm
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// Archivos locales de la app
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// CSS Global
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Configuracion extra
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
+// Instalacion principal de Vue
 new Vue({
   router,
   store,
