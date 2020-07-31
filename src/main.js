f5b46e4865a91b2cc1544961bd405c7151de2467
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-07-31 13:52:09 -0500

    Refactorizando

--- src/main.js
+++ src/main.js
@@ -1,43 +1,22 @@
 // Paquetes de npm
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
+
+// BootstrapVue
+import './plugins/BootstrapVue'
+
+// Vue Font-Awesome
+import './plugins/fontAwesome'
 
 // Archivos locales de la app
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-// Iconos de tipo "Solid"
-import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
-// Iconos de tipo "Brand" (marcas o logos de empresas)
-import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
-// El componente que vamos a utilizar
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 // CSS Global
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/main.styl'
 
-library.add(
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-)
-
 // Configuracion extra
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // Instalacion principal de Vue
 new Vue({
