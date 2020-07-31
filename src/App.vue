a860f552986790611e211ed6a6d674e788332598
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-07-31 12:32:42 -0500

    Se crean los Layouts principales y se establece cuando se muestra cada uno

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Loadlayout v-if="isLoading">
+      <BaseLoading/>
+    </Loadlayout>
+
+    <MainLayout v-else/>
+
   </div>
 </template>
 
+<script>
+import { mapState } from 'vuex'
+import Loadlayout from './layouts/LoadLayout'
+import MainLayout from './layouts/MainLayout'
+import BaseLoading from '@/components/BaseLoading.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainLayout,
+    Loadlayout,
+    BaseLoading
+  },
+  computed: {
+    // Uso: mapState(moduleName, { state })
+    ...mapState('loading', { isLoading: 'isLoading' })
+  }
+}
+</script>
+
 <style lang="stylus">
 #app
+  padding 60px 0
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
   color #ffffff
   background-color #15202b
-  margin-top 60px
 </style>
