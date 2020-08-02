55badf47ae3f0bab6798cb84caa3a9f7cb534b1f
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-08-02 11:36:09 -0500

    Stats y tiempo jugado

--- src/views/Profile/MainBlock/Index.vue
+++ src/views/Profile/MainBlock/Index.vue
@@ -8,7 +8,7 @@
       <ProgressList :acts="profileData.progression"/>
     </div>
     <div class="grid-item item-right">
-      <h1>Derecha</h1>
+      <PlayerStats :stats="statsData"/>
     </div>
   </div>
 </template>
@@ -17,10 +17,11 @@
 import TopHeroes from './TopHeroes/Index'
 import HeroesList from './HeroesList/Index'
 import ProgressList from './ProgressList/Index'
+import PlayerStats from './PlayerStats/Index'
 
 export default {
   name: 'MainBlock',
-  components: { TopHeroes, HeroesList, ProgressList },
+  components: { TopHeroes, HeroesList, ProgressList, PlayerStats },
   props: {
     profileData: {
       type: Object,
@@ -40,6 +41,10 @@ export default {
     },
     heroesList () {
       return this.profileData.heroes.slice(3, this.profileData.heroes.length)
+    },
+    statsData () {
+      const { paragonLevel, kills, timePlayed } = this.profileData
+      return { paragonLevel, kills, timePlayed }
     }
   }
 }
