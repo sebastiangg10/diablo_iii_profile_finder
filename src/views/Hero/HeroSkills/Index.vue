b84b93ee49a90bb6e8bc58d65f276f13eac6232b
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-08-02 15:40:34 -0500

    Componentes dinamicos, asincronos, dinamicos asincronos y keep alive

--- src/views/Hero/HeroSkills/Index.vue
+++ src/views/Hero/HeroSkills/Index.vue
@@ -3,27 +3,58 @@
     <h2 class="font-diablo">Skills</h2>
     <hr class="bg-white">
 
-    <ActiveSkills :skills="skills.active"/>
-    <hr>
-    <PassiveSkills :skills="skills.passive"/>
+    <b-nav pills small>
+      <b-nav-item :active="!isPassiveSkillsActive"
+      @click="changeComponent('ActiveSkills')">Active</b-nav-item>
+      <b-nav-item :active="isPassiveSkillsActive"
+      @click="changeComponent('PassiveSkills')">Passive</b-nav-item>
+    </b-nav>
 
+    <keep-alive>
+      <component :is="activeComponent" :skills="componentProps"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import ActiveSkills from './ActiveSkills'
-import PassiveSkills from './PassiveSkills'
+// import ActiveSkills from './ActiveSkills'
+// import PassiveSkills from './PassiveSkills'
 
 export default {
   name: 'HeroSkills',
   components: {
-    ActiveSkills,
-    PassiveSkills
+    ActiveSkills: () => import(/* webpackChunkName: "ActiveSkills" */'./ActiveSkills'),
+    PassiveSkills: () => import(/* webpackChunkName: "PassiveSkills" */'./PassiveSkills')
   },
   props: {
     skills: {
       required: true,
       type: Object
+    }
+  },
+  data () {
+    return {
+      activeComponent: 'ActiveSkills'
+    }
+  },
+  methods: {
+    changeComponent (component) {
+      this.activeComponent = component
+    }
+  },
+  computed: {
+    /**
+     * Dinamyc props for async dynamic components
+     * @returns {String}
+     */
+    // Con esto estamos generando "props" dinámicas
+    // Si el componente es ActiveSkills pasa como props las activas, si no, las pasivas
+    componentProps () {
+      return this.activeComponent === 'ActiveSkills' ? this.skills.active : this.skills.passive
+    },
+    // Nos dice si el componente "HabilidadesPasivas" está activo o no
+    isPassiveSkillsActive () {
+      return this.activeComponent === 'PassiveSkills'
     }
   }
 }
